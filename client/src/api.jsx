68f4fda0c8869ec715ec7e487c6f712a332dc06b
riteshkaul <riteshkaul682@gmail.com>
2024-11-14 22:55:01 +0530

    login change

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // Adjust to your server's base URL
+  baseURL: "https://spyne-assignment-3.onrender.com/api", // Adjust to your server's base URL
   withCredentials: true,
 });
 
